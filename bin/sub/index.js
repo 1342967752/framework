7800709d12e21456f31c06114231185784c75fcf
xs <1342967752@qq.com>
2021-09-09 18:11:22 +0800

    1

---
+++ bin/sub/index.js
@@ -0,0 +1,8 @@
+//-----libs-begin-----
+loadLib("libs/laya.core.js")
+loadLib("libs/laya.d3.js")
+loadLib("libs/laya.ui.js")
+loadLib("libs/laya.physics3D.js")
+//-----libs-end-------
+
+loadLib("js/bundle.js");
